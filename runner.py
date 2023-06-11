bca8352c1991645105a68cd41d3daf8a537697ed
Ayin49 <doskii@o2.pl>
2023-06-11 07:58:53 +0200

    changing storage

--- runner.py
+++ runner.py
@@ -149,5 +149,5 @@ import numpy as np
 for (alpha, beta, gamma) in product(np.arange(1, 8, 1), np.arange(0, 1.2, 0.2), np.arange(0, 2.2, 0.2)):
     print((alpha, beta, gamma))
     traci.start([checkBinary('sumo'), "-c", "C:\\Users\\Ayin\\PycharmProjects\\SwarmTraffic\\data2\\grid66.sumocfg",
-                 "--tripinfo-output", ".\\stats\\tripinfo_a_{}_b_{}_g_{}.xml".format(alpha, beta, gamma)])
+                 "--tripinfo-output", ".\\stats\\tripinfo_a_{}_b_{}_g_{}_.xml".format(alpha, beta, gamma)])
     run(alpha, beta, gamma)
